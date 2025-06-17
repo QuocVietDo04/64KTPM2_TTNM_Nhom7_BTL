e68826fe4a3529eef28520967fb9ff2570656b5c
duckmoiz040 <dqviet204@gmail.com>
2025-06-17 17:06:06 +0700

    Reset Folder Root

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="header__logo">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
